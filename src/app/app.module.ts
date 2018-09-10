3787224c87b6085eb3c2faf264ef1205ac2289ae
Kelly Villalobos <kvdesign1978@gmail.com>
2018-09-09 22:29:00 -0500

    navlink toggle
    toggleing nav links active by adding click event and setting ngClass to active when they are clicked.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,18 @@ import { RecipeListDetailComponent } from './recipes/recipe-detail/recipe-detail
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: 'recipe',
+    component: RecipesComponent
+  },
+  {
+    path: 'shopping-list',
+    component: RecipeListComponent
+  }
+];
 
 @NgModule({
   declarations: [
