918a1077aa12cffbd0f39ca533b1c4e5b3433d16
Kelly Villalobos <kvdesign1978@gmail.com>
2018-09-09 21:16:31 -0500

    containers
    placed some container divs arround components for some space.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/06/29/20/09/mexican-2456038_1280.jpg')
+    new Recipe('A Test Recipe 2', 'This is simply a test', 'https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg')
   ];
   constructor() { }
 
