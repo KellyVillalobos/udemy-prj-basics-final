f54c0d09e0c8ef1f34249b0b5464a9d179c9e0fb
Kelly Villalobos <kvdesign1978@gmail.com>
2018-09-26 22:31:45 -0500

    small adjustment to titles

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output()recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg'),
-    new Recipe('A Test Recipe 2', 'This is simply a test', 'https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg')
+    new Recipe('Shrimp Au Tufe', 'A Cajun Delicacy', 'https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg'),
+    new Recipe('Kung Pao Chicken', 'The Best Chineese Dish', 'https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg')
   ];
   constructor() { }
 
